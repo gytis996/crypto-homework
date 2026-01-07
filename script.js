c490489a151ea9927330635fc7410c464e47adc1
gytis996 <pliumbt1@gmail.com>
2026-01-07 12:54:59 +0200

    Baigtas

---
+++ script.js
@@ -0,0 +1,41 @@
+const cryptoWrapper = document.getElementById("crypto-wrapper");
+
+const buildScreen = async () => {
+  const response = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
+  );
+  const coins = await response.json();
+
+  coins.forEach((coin) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    coins.sort((a, b) => a.name.localeCompare(b.name));
+
+    coins.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+
+    const title = document.createElement("h2");
+    title.innerText = `${coin.name} (${coin.symbol.toUpperCase()})`;
+
+    const price = document.createElement("h4");
+    price.textContent = `Price: $${coin.current_price}`;
+
+    if (coin.current_price >= 100) {
+      card.classList.add("expensive");
+    } else {
+      card.classList.add("shitcoin");
+    }
+
+    card.addEventListener("click", () => {
+      console.log(coin.name);
+    });
+
+    card.append(title, price);
+    cryptoWrapper.appendChild(card);
+  });
+};
+
+buildScreen();
